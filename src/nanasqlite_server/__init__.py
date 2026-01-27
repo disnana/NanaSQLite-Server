e21465c38cfb3396614b1849bd8eb1923f8a7226
harumaki4649 <support@disnana.com>
2026-01-27 22:33:00 +0900

    v1.1.1正式リリース

--- src/nanasqlite_server/__init__.py
+++ src/nanasqlite_server/__init__.py
@@ -4,6 +4,6 @@ NanaSQLite Server: A secure QUIC-based RPC server for NanaSQLite.
 
 from .server import main, main_sync
 
-__version__ = "1.1.1dev2"
+__version__ = "1.1.1"
 __author__ = "NanaSQLite-Project"
 __all__ = ["main", "main_sync"]
